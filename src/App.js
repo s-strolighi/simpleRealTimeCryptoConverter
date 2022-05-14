2862bf35718037048dc77881a516df5a8e3b4b1c
Sandro Strolighi <sandro.strolighi@gmail.com>
2022-05-14 18:24:12 +0200

    Aggiunta selezione card mint

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Navbar, Container, Card, InputGroup, FormControl, Button } from "react-bootstrap";
+import { Navbar, Container, Card, InputGroup, FormControl, Button, Form, Dropdown, DropdownButton } from "react-bootstrap";
 import logo from './logo512.png';
 
 const App = () => {
@@ -10,6 +10,7 @@ const App = () => {
   const cryptoIdList = ["green-satoshi-token", "stepn", "solana"]
   const fiatIdList = ["eur", "usd"]
   const [currentGstUsdPrice, setCurrentGstUsdPrice] = useState(0)
+  const [currentMintKey, setCurrentMintKey] = useState(null)
 
   const symbols = { 'green-satoshi-token': 'GST', 'stepn': 'GMT', 'solana': 'SOL' }
   const [symbolsValue, setSymbolsValue] = useState({
@@ -20,19 +21,18 @@ const App = () => {
   const [usdEurRateo, setUsdEurRateo] = useState(0)
 
   const [allValues, setAllValues] = useState({})
-  const [lastChanged, setLastChanged] =  useState(null)
   const [mintValues, setMintValues] = useState({
-    "200-0": { "eur": 0, "sol": 0 },
-    "160-40": { "eur": 0, "sol": 0 },
-    "120-80": { "eur": 0, "sol": 0 },
-    "100-100": { "eur": 0, "sol": 0 },
-    "80-120": { "eur": 0, "sol": 0 },
-    "40-160": { "eur": 0, "sol": 0 }
+    "200/0": { "eur": 0, "sol": 0 },
+    "160/40": { "eur": 0, "sol": 0 },
+    "120/80": { "eur": 0, "sol": 0 },
+    "100/100": { "eur": 0, "sol": 0 },
+    "80/120": { "eur": 0, "sol": 0 },
+    "40/160": { "eur": 0, "sol": 0 }
   })
   const [levelCost, setLevelCost] = useState({ "5": { "eur": 0, "sol": 0 } })
 
   const updatePeriodicallySymbolsValues = () => {
-    setInterval(updateSymbolsValues, 15*1000)
+    setInterval(updateSymbolsValues, 15 * 1000)
   }
 
   const updateSymbolsValues = (init = false) => {
@@ -55,7 +55,7 @@ const App = () => {
           setSymbolsValue(symbolsValueUpdated);
           setUsdEurRateo(usdEurRateoUpdated);
           updateMintStats();
-          if(init)
+          if (init)
             handleChangeText('green-satoshi-token', dailyGstValue);
         })
       })
@@ -100,7 +100,7 @@ const App = () => {
         allValuesUpdated[key] = parseFloat(amount) / symbolsValue[key][symbol]
         return true;
       })
-      if(done){
+      if (done) {
         if (symbol === 'eur') {
           allValuesUpdated.eur = amount
           allValuesUpdated.usd = amount * usdEurRateo
@@ -111,162 +111,181 @@ const App = () => {
         }
       }
     }
-  if (done)
-    setAllValues(allValuesUpdated)
-}
-
-const updateMintStats = () => {
-  let mintValuesUpdated = mintValues;
-  let levelCostUpdated = levelCost;
-
-  let done = Object.keys(mintValues).every(value => {
-    let key = value
-    let gstAmount = parseInt(value.split("-")[0].trim())
-    let gmtAmount = parseInt(value.split("-")[1].trim())
-
-    mintValuesUpdated[key].eur = symbolsValue["green-satoshi-token"].eur * gstAmount + symbolsValue["stepn"].eur * gmtAmount
-    mintValuesUpdated[key].sol = mintValuesUpdated[key].eur / symbolsValue["solana"].eur
-
-    return true
-  })
-
-  if (done) {
-    levelCostUpdated[5].eur = symbolsValue["green-satoshi-token"].eur * 20 + symbolsValue["stepn"].eur * 10
-    levelCostUpdated[5].sol = levelCostUpdated[5].eur / symbolsValue["solana"].eur
-
-    setLevelCost(levelCostUpdated)
-    setMintValues(mintValuesUpdated)
-    setCurrentGstUsdPrice(symbolsValue["green-satoshi-token"].usd)
+    if (done)
+      setAllValues(allValuesUpdated)
   }
 
-}
+  const updateMintStats = () => {
+    let mintValuesUpdated = mintValues;
+    let levelCostUpdated = levelCost;
 
-function customRound(value) {
-  return Math.round((value + Number.EPSILON) * 100) / 100
-}
+    let done = Object.keys(mintValues).every(value => {
+      let key = value
+      let gstAmount = parseInt(value.split("/")[0].trim())
+      let gmtAmount = parseInt(value.split("/")[1].trim())
 
-function CardMinting() {
-  let key;
-  let message;
-  let price = currentGstUsdPrice;
-  switch (true) {
-    case (price < 2):
-      key = "200-0"
-      message = "GST lower than 2 USD"
-      break;
-    case (price < 3):
-      key = "160-40"
-      message = "GST between 2 and 3 USD"
-      break;
-    case (price < 4):
-      key = "120-80"
-      message = "GST between 3 and 4 USD"
-      break;
-    case (price < 8):
-      key = "100-100"
-      message = "GST between 4 and 8 USD"
-      break;
-    case (price < 10):
-      key = "80-120"
-      message = "GST between 8 and 10 USD"
-      break;
-    case (price >= 10):
-      key = "40-160"
-      message = "GST higher than 10 USD"
-      break;
-    default:
-      key = "Error"
-      break;
+      mintValuesUpdated[key].eur = symbolsValue["green-satoshi-token"].eur * gstAmount + symbolsValue["stepn"].eur * gmtAmount
+      mintValuesUpdated[key].sol = mintValuesUpdated[key].eur / symbolsValue["solana"].eur
+
+      return true
+    })
+
+    if (done) {
+      levelCostUpdated[5].eur = symbolsValue["green-satoshi-token"].eur * 20 + symbolsValue["stepn"].eur * 10
+      levelCostUpdated[5].sol = levelCostUpdated[5].eur / symbolsValue["solana"].eur
+
+      setLevelCost(levelCostUpdated)
+      setMintValues(mintValuesUpdated)
+      setCurrentGstUsdPrice(symbolsValue["green-satoshi-token"].usd)
+    }
+
   }
 
-  let gstAmount = key.split('-')[0].trim()
-  let gmtAmount = key.split('-')[1].trim()
-  let totalSolPrice = customRound(mintValues[key].sol + levelCost[5].sol)
-  let totalEurPrice = customRound(mintValues[key].eur + levelCost[5].eur)
-  return <Card className="mx-auto m-4">
-    <Card.Header as="h5">Is minting worth?</Card.Header>
-    <Card.Body>
-      {
-        price ?
-          <div>
-            <small className="text-muted">{message} - {price} $</small>
-            <h5 className="mt-1">{gstAmount}/{gmtAmount} + level5 = <b>{totalEurPrice}€</b> = <b>{totalSolPrice}SOL</b></h5>
-            <small className="text-muted">{"(floor price including 6% fee)"}</small><br/>
-            <h5>Sell price = <b>{customRound(totalEurPrice/0.94)}€</b> = <b>{customRound(totalSolPrice/0.94)}SOL</b></h5><br/>
-            <b>Mint</b>: {gstAmount}GST+{gmtAmount}GMT = <b>{customRound(mintValues[key].eur)}€</b> = <b>{customRound(mintValues[key].sol)}SOL</b><br />
-            <b>Level5</b>: 20GST+10GMT = <b>{customRound(levelCost[5].eur)}€</b> = <b>{customRound(levelCost[5].sol)}SOL</b><br />
+  function customRound(value) {
+    return Math.round((value + Number.EPSILON) * 100) / 100
+  }
+
+  function CardMinting() {
+    let mintKey;
+    let message;
+    let price = currentGstUsdPrice;
+    switch (true) {
+      case (price < 2):
+        mintKey = currentMintKey || "200/0"
+        message = "GST lower than 2 USD"
+        break;
+      case (price < 3):
+        mintKey = currentMintKey || "160/40"
+        message = "GST between 2 and 3 USD"
+        break;
+      case (price < 4):
+        mintKey = currentMintKey || "120/80"
+        message = "GST between 3 and 4 USD"
+        break;
+      case (price < 8):
+        mintKey = currentMintKey || "100/100"
+        message = "GST between 4 and 8 USD"
+        break;
+      case (price < 10):
+        mintKey = currentMintKey || "80/120"
+        message = "GST between 8 and 10 USD"
+        break;
+      case (price >= 10):
+        mintKey = currentMintKey || "40/160"
+        message = "GST higher than 10 USD"
+        break;
+      default:
+        mintKey = currentMintKey || "Error"
+        break;
+    }
+
+    let gstAmount = mintKey.split('/')[0].trim()
+    let gmtAmount = mintKey.split('/')[1].trim()
+    let totalSolPrice = customRound(mintValues[mintKey].sol + levelCost[5].sol)
+    let totalEurPrice = customRound(mintValues[mintKey].eur + levelCost[5].eur)
+    return <Card className="mx-auto m-4">
+      <Card.Header as="h5">
+        <div className="row">
+          <div className="col col-lg-4">
+            Is minting worth?
+
           </div>
-          : <div className="d-flex justify-content-center">
-            <div className="spinner-border" role="status">
+          <div className="col col-lg-4">
+            <DropdownButton title={gstAmount+"GST+"+gmtAmount+"GMT"} size="sm">
+              <Dropdown.Item as="button" onClick={() => setCurrentMintKey("200/0")}><b>{200}</b>GST+<b>{0}</b>GMT {"(GST < 2$)"}</Dropdown.Item>
+              <Dropdown.Item as="button" onClick={() => setCurrentMintKey("160/40")}><b>{160}</b>GST+<b>{40}</b>GMT {"(GST 2-3$)"}</Dropdown.Item>
+              <Dropdown.Item as="button" onClick={() => setCurrentMintKey("120/80")}><b>{120}</b>GST+<b>{80}</b>GMT {"(GST 3-4$)"}</Dropdown.Item>
+              <Dropdown.Item as="button" onClick={() => setCurrentMintKey("100/100")}><b>{100}</b>GST+<b>{100}</b>GMT {"(GST 4-8$)"}</Dropdown.Item>
+              <Dropdown.Item as="button" onClick={() => setCurrentMintKey("80/120")}><b>{80}</b>GST+<b>{120}</b>GMT {"(GST 8-10$)"}</Dropdown.Item>
+              <Dropdown.Item as="button" onClick={() => setCurrentMintKey("40/160")}><b>{40}</b>GST+<b>{160}</b>GMT {"(GST > 10$)"}</Dropdown.Item>
+
+            </DropdownButton>
+          </div>
+        </div>
+
+      </Card.Header>
+      <Card.Body>
+        {
+          price ?
+            <div>
+              <small className="text-muted">{message} - {price} $</small>
+              <h5 className="mt-1">{mintKey} + level5 = <b>{totalEurPrice}€</b> = <b>{totalSolPrice}SOL</b></h5>
+              <small className="text-muted">{"(floor price including 6% fee)"}</small><br />
+              <h5>Sell price = <b>{customRound(totalEurPrice / 0.94)}€</b> = <b>{customRound(totalSolPrice / 0.94)}SOL</b></h5><br />
+              <b>Mint</b>: {gstAmount}GST+{gmtAmount}GMT = <b>{customRound(mintValues[mintKey].eur)}€</b> = <b>{customRound(mintValues[mintKey].sol)}SOL</b><br />
+              <b>Level5</b>: 20GST+10GMT = <b>{customRound(levelCost[5].eur)}€</b> = <b>{customRound(levelCost[5].sol)}SOL</b><br />
             </div>
-          </div>
-      }
-      <br />
-    </Card.Body>
-  </Card>
-}
+            : <div className="d-flex justify-content-center">
+              <div className="spinner-border" role="status">
+              </div>
+            </div>
+        }
+        <br />
+      </Card.Body>
+    </Card>
+  }
 
-useEffect(() => {
-  updateSymbolsValues(true)
-  updatePeriodicallySymbolsValues();
-}, [])
+  useEffect(() => {
+    updateSymbolsValues(true)
+    updatePeriodicallySymbolsValues();
+  }, [])
 
-return <>
-  <Navbar bg="primary" variant="dark">
-    <Container>
-      <Navbar.Brand>
-        <img
-          alt=""
-          src={logo}
-          width="50"
-          height="50"
-          className="d-inline-block"
-        />{' '}
-        Simple real time crypto converter
-      </Navbar.Brand>
-    </Container>
-  </Navbar>
-  <div className="mx-auto row mainContainer">
-    <div className="col-lg">
-      <Card className="mx-auto m-4">
-        <Card.Header as="h5">Real time converter</Card.Header>
-        <Card.Body>
-          <Card.Title className="mb-3">Select your crypto or change them</Card.Title>
-          <div className="row">
-            {
-              cryptoIdList.map((value, index) => {
-                return <div key={'crypto' + index} className="col-lg mb-3">
-                  <InputGroup>
-                    <Button variant="outline-primary">{symbols[value]}</Button>
-                    <FormControl placeholder="insert" value={allValues[value] || ''} onChange={(e) => handleChangeText(value, e.target.value)} />
-                  </InputGroup>
-                </div>
-              })
-            }
-            <hr />
-            {
-              fiatIdList.map((value, index) => {
-                return <div key={'fiat' + index} className="col-lg mb-3">
-                  <InputGroup>
-                    <Button variant="outline-primary">{value.toUpperCase()}</Button>
-                    <FormControl placeholder="insert" value={allValues[value] || ''} onChange={(e) => handleChangeText(value, e.target.value)} />
-                  </InputGroup>
-                </div>
-              })
-            }
+  return <>
+    <Navbar bg="primary" variant="dark">
+      <Container>
+        <Navbar.Brand>
+          <img
+            alt=""
+            src={logo}
+            width="50"
+            height="50"
+            className="d-inline-block"
+          />{' '}
+          Simple real time crypto converter
+        </Navbar.Brand>
+      </Container>
+    </Navbar>
+    <div className="mx-auto row mainContainer">
+      <div className="col-lg">
+        <Card className="mx-auto m-4">
+          <Card.Header as="h5">Real time converter</Card.Header>
+          <Card.Body>
+            <Card.Title className="mb-3">Select your crypto or change them</Card.Title>
+            <div className="row">
+              {
+                cryptoIdList.map((value, index) => {
+                  return <div key={'crypto' + index} className="col-lg mb-3">
+                    <InputGroup>
+                      <Button variant="outline-primary">{symbols[value]}</Button>
+                      <FormControl placeholder="insert" value={allValues[value] || ''} onChange={(e) => handleChangeText(value, e.target.value)} />
+                    </InputGroup>
+                  </div>
+                })
+              }
+              <hr />
+              {
+                fiatIdList.map((value, index) => {
+                  return <div key={'fiat' + index} className="col-lg mb-3">
+                    <InputGroup>
+                      <Button variant="outline-primary">{value.toUpperCase()}</Button>
+                      <FormControl placeholder="insert" value={allValues[value] || ''} onChange={(e) => handleChangeText(value, e.target.value)} />
+                    </InputGroup>
+                  </div>
+                })
+              }
 
-          </div>
-        </Card.Body>
-      </Card>
+            </div>
+          </Card.Body>
+        </Card>
+      </div>
+      <div className="col-lg">
+        <CardMinting />
+      </div>
     </div>
-    <div className="col-lg">
-      <CardMinting />
-    </div>
-  </div>
 
 
 
-</>
+  </>
 }
 
 export default App;
